c3009150556667505249836b4aabc42283340868
zosia <zofiaszczech1@gmail.com>
2017-03-16 00:17:28 +0100

    new files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var h = prompt('wpisz wysokość trójkąta');
+var	a = prompt('wpisz podstawę trójkąta');
+//var h = 4;
+//var	a = 2;
+var triangleArea = (a * h)/2;
+
+
+console.log(triangleArea);
+
+
